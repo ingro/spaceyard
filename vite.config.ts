04841b00beb8ac599d24982e09ad4c1152344ccd
Emanuele Ingrosso <ingro85@gmail.com>
2021-03-15 13:01:24 +0100

    Improved build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import typescript2 from 'rollup-plugin-typescript2'
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    {
+      ...typescript2({
+        check: false,
+        tsconfigOverride: {
+          include: ['./lib'],
+          compilerOptions: {
+            declaration: true
+          }
+        }
+      }),
+      apply: 'build'
+    }
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/main.ts'),
       name: 'SpaceYard'
     },
     rollupOptions: {
-      external: ['react', 'react-dom']
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
     }
   },
   server: {
