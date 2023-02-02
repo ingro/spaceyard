a76fb98850c679e11e5fd1430dc158eb53c0b110
Emanuele Ingrosso <ingro85@gmail.com>
2023-02-02 14:34:49 +0100

    Rimosso path-to-regexp e utilizzato al suo posto helper generatePath di react-router

--- lib/utilities/routes.ts
+++ lib/utilities/routes.ts
@@ -1,14 +1,17 @@
-import { compile } from 'path-to-regexp';
+// import { compile } from 'path-to-regexp';
+import { generatePath } from 'react-router';
 import qs from 'query-string';
 
 export function createRouteHelper(routesList: Record<string, string>, homePath: string = '/') {
     return {
         getUrlForRoute(routeName: string, params: any = {}, query: any = {}): string {
-            const compiler = compile(routesList[routeName]);
+            // const compiler = compile(routesList[routeName]);
 
             const queryString = qs.stringify(query);
 
-            const compiled = compiler(params);
+            // const compiled = compiler(params);
+
+            const compiled = generatePath(routesList[routeName], params);
 
             if (queryString === '') {
                 return compiled;
